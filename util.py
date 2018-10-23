5cdbdf85fc13c66995544657b848c923696272b7
Ritoban Roy-Chowdhury <ritobanrc@gmail.com>
2018-10-23 13:07:35 -0700

    added util.py and number/letter conversion functions

---
+++ util.py
@@ -0,0 +1,14 @@
+#!/bin/python3
+
+def n2l(n):
+    return chr(n+65)
+
+def l2n(l):
+    return ord(l)-65
+
+
+if __name__ == '__main__':
+    print('A: ', n2l(0))
+    print('N: ', n2l(13))
+    print('23: ', l2n('X'))
+    print('1: ', l2n('B'))

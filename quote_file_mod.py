26fc6416251095bcc487f5e4d9f655876fb9611e
anSEpranks <khaitvu@gmail.com>
2018-11-07 10:37:19 -0800

    added a function to get a single quote

--- quote_file_mod.py
+++ quote_file_mod.py
@@ -6,7 +6,8 @@ def get_quotes(file_name="randomquotes.csv"):
         dat = csv.reader(f, delimiter=';')
         e=[r[0]+' - '+ r[1] for r in dat]
         return e
-
+def get_quote():
+    return choice(get_quotes())
 if __name__ == "__main__":
     t = get_quotes()
     for i in range(10):

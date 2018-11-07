24921b39bb1a82b106a6e7795bffb801c7733334
Ritoban Roy-Chowdhury <ritobanrc@gmail.com>
2018-11-07 14:51:19 -0800

    renamed caesar

---
+++ caesar.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+from util import *
+from random import randint
+from quote_file_mod import get_quote
+
+def get_alphabet(shift = 0):
+    r = [n2l(l + shift) for l in range(0, 26)]
+    return r
+
+
+def caesar_cipher(message, shift, keep_spaces=True):
+    return ''.join([l2n2ls(l,shift) # calculate the letter after shift
+                    if l != ' ' # but only if the letter is not a space.
+                    # if it is a space, insert a space or nothing, based on
+                    # keep_spaces
+                    else (' ' if keep_spaces else '')
+                    for l in message]) # for each letter in message
+
+
+if __name__ == '__main__':
+    print(get_alphabet())
+    print(get_alphabet(randint(0,25)))
+    t = get_quote()
+    print(t)
+    print(caesar_cipher(t, randint(1,25)))
